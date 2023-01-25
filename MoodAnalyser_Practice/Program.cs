038d32e33f41d25aa7d0dbc9035d4e8acb0d6006
Rutuja412 <rutujakadam4545@gmail.com>
2023-01-25 13:45:22 +0530

    [Rutuja]:Welcome

---
+++ MoodAnalyser_Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyser_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Mood Analyser !!");
+        }
+    }
+}
